d2dca6f1f6bb3ae91618a5d99ba5cb4bcb5d01d5
LENDOLPE <chan82600@naver.com>
2022-06-16 17:26:40 +0900

    22-06-16

--- PrecticePlace/JScode/Prectice2.js
+++
@@ -1,8 +0,0 @@
-const letters = ['D', 'C', 'E', 'B', 'A'];
-const numbers = [1, 10, 4, 21, 36000];
-
-letters.sort();
-numbers.sort();
-
-console.log(letters);
-console.log(numbers);
---
+++ menu-choice/Jscode/functions.js
@@ -0,0 +1,5 @@
+import addMenu from './functionjs/add.js';
+import deleteMenu from './functionjs/delete.js';
+import rollMenu from './functionjs/roll.js';
+
+export { addMenu, deleteMenu, rollMenu };
